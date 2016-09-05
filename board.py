9695883a1d64373af7fda1a734a12d612f75c681
satyeshmundra <satyesh.mundra@gmail.com>
2016-09-05 23:17:30 +0530

    Added code for genrating board

---
+++ board.py
@@ -0,0 +1,13 @@
+
+n = input("Enter the size of the board \n")
+
+for i in range (n):
+	for j in range (n):
+		print(" ---"),
+	print("\n")
+	for j in range (n+1) :
+		print("|   "),
+	print ("\n")
+
+for i in range  (n):
+	print (" ---"),
